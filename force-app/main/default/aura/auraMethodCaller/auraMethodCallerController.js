50b37442dfeb87a1f0a202445d6fd35fccd3374e
Manas Datta <manasdatta.cs@gmail.com>
2020-05-11 13:25:28 +0800

    commit 2

--- force-app/main/default/aura/auraMethodCaller/auraMethodCallerController.js
+++ force-app/main/default/aura/auraMethodCaller/auraMethodCallerController.js
@@ -7,4 +7,15 @@
           childCmp.logParam("message sent by parent component");
         console.log("auraMethodResult: " + auraMethodResult);
     },
+
+    callAuraMethodServerTrip : function(component, event, helper) {
+        var childCmp = component.find("child2");
+        // call the aura:method in the child component
+        childCmp.echo(function(result) {
+            console.log("callback for aura:method was executed");
+            console.log("result: " + result);
+        });
+    },
+
+
 })
